89ef379029614a8510658f1d707eff181bb148f9
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-23 13:10:34 +0600

    inerview questions added

---
+++ interview-questions.js
@@ -0,0 +1,12 @@
+/* 
+1. What is ES6? Have you ever use anything form ES6?
+2. Explain the difference between var let and const.
+3. What is the arrow function, and how to create it?
+4. Give an example of an Arrow function in ES6? List down it advantages.
+5. Discuss spread operator in ES6 with an example.
+6. What do you understand about default parameters?
+7. What are template literals in ES6?
+8. Tell us the difference between arrow and regular function.
+9. Tell us the difference between for.. of and for ...in.
+
+*/
